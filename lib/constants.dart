89502129921e18d766b26412544c6b5e52516fb5
dnettey3 <dnettey3@gmail.com>
2020-05-11 19:11:37 +0000

    Testing NFC

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+  // HIVE KEYS AND CONSTANTS
+
+  const String MAINBOX="mainBox";
+  const String LOGGEDIN="loggedIn";
+  const String TOKEN="token";
+  const String MARKETPLACEMODAL="wMPModal";
+  const String WALLETMODAL="wWModal";
+  const String DISCOVERMODAL="wDModal";
+  const String GAMESTOREMODAL="wGModal";
+  const String INSPIRATIONMODAL="wIModal";
